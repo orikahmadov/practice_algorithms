9b94424ace5ad1d9666035f5f8ebf5883372d9b8
Orkhan Ahmadov <55695784+orikahmadov@users.noreply.github.com>
2021-02-07 21:33:49 +0100

    Update Anatomy_of_Loop.py

--- Anatomy_of_Loop.py
+++ Anatomy_of_Loop.py
@@ -11,3 +11,14 @@ while True:
         break
     else:
         continue
+
+        
+#NESTED LOOPS E.G BUBBLE SORT
+
+list = [10,8,6,5,3,2,1]
+for i in range(len(list)- 1):           #outer loop starts from 0 ---> to -1 one before the last item of the list
+    for j in range(i + 1, len(list)):   #inner loop! The "inner loop" will be executed one time for each iteration of the "outer loop"
+        if list[i] > list[j]:
+            list[j],list[i] = list[i], list[j]
+ 
+    
